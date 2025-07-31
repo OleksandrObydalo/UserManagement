4b4521875cf30201b9ead4a7580da566567c7934
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-31 13:08:04 +0300

    Add constuctors of User class with automatic id generation

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class User {
+public class User {
+    private int id;
+    private String name;
+    private String password;
+
+    private static int counter = 1;
+
+    public User() {
+        this.id = counter;
+        counter++;
+    }
+
+    public User(String name, String password) {
+        this();
+        this.name = name;
+        this.password = password;
+    }
 }
