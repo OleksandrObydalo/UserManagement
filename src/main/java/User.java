0234211f1e6986796658a79d8d068010690e1ac0
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-31 13:09:14 +0300

    Add getters and setters

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -15,4 +15,20 @@ public class User {
         this.name = name;
         this.password = password;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
